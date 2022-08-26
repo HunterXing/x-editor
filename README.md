fdde93287658a6d315d94200c6653dd8e40ad2a6
xing.heng <xing.heng@ustcinfo.com>
2022-08-26 15:18:32 +0800

    feat: 初始化electron项目

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # x-editor
 Markdown editor on desktop
+
+## 技术架构
+采用 electron react
+
+
+## 参考文档
+[electron](https://www.electronjs.org/zh/docs/latest/tutorial/quick-start)
---
+++ src/elec/main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile(path.join(__dirname, 'index.html'))
+
+  // 打开开发工具
+  win.webContents.openDevTools()
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
