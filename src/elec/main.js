b40caf4ed8e74806d4c0dcfba29a9055febd76fa
xing.heng <xing.heng@ustcinfo.com>
2022-08-26 21:44:15 +0800

    fix: 删除不必要文件

--- src/elec/main.js
+++ src/elec/main.js
@@ -3,8 +3,8 @@ const path = require('path')
 
 function createWindow () {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1000,
+    height: 800,
     webPreferences: {
       nodeIntegration: true,
       preload: path.join(__dirname, 'preload.js')
@@ -14,7 +14,7 @@ function createWindow () {
   // win.loadFile(path.join(__dirname, 'index.html'))
   win.loadURL('http://localhost:3000')
   // 打开开发工具
-  // win.webContents.openDevTools()
+  win.webContents.openDevTools()
 }
 
 app.whenReady().then(() => {
